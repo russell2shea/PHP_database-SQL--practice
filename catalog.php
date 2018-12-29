10d2c66690d1dbfedb49c10e5e458d5ec801070e
Russell Shea <russell2shea@gmail.com>
2018-12-29 15:55:27 -0500

    Added pagination

--- catalog.php
+++ catalog.php
@@ -1,9 +1,12 @@
-<?php 
+<?php
 include("inc/functions.php");
 
 $pageTitle = "Full Catalog";
 $section = null;
+$items_per_page = 8;
 
+
+/* set the category ifset */
 if (isset($_GET["cat"])) {
     if ($_GET["cat"] == "books") {
         $pageTitle = "Books";
@@ -17,36 +20,85 @@ if (isset($_GET["cat"])) {
     }
 }
 
-//if section is empty call the full array else call just the category array
-if(empty($section)){
-    $catalog = full_catalog_array();
-} else{
-    $catalog = category_catalog_array($section);
+/* page number is set get the page number */
+if (isset($_GET["pg"])) {
+    $current_page = filter_input(INPUT_GET,"pg",FILTER_SANITIZE_NUMBER_INT);
 }
 
+$total_items = get_catalog_count($section);
+$total_pages = ceil($total_items / $items_per_page);
+$limit_results = "";
+
+/* limit results in redirect */
+if (!empty($section)) {
+    $limit_results = "cat=" . $section . "&";
+}
+
+/* redirect too-large page numbers to the last page */
+if ($current_page > $total_pages) {
+    header("location:catalog.php?"
+        . $limit_results
+        . "pg=".$total_pages);
+}
+
+/* redirect too-small page numbers to the first page */
+if ($current_page < 1) {
+    header("location:catalog.php?"
+        . $limit_results
+        . "pg=1");
+}
+
+/* determine the offset (number of items to skip) for the current page */
+$offset = ($current_page - 1) * $items_per_page;
+
+
+/* if section is empty call the full array else call just the category array */
+if (empty($section)) {
+    $catalog = full_catalog_array($items_per_page,$offset);
+} else {
+    $catalog = category_catalog_array($section,$items_per_page,$offset);
+}
+
+$pagination = "<div class=\"pagination\">";
+$pagination .= "Pages: ";
+
+    for($i=1;$i<=$total_pages;$i++){
+        if($i == $current_page){
+            $pagination .= " <span>$i</span>";
+        } else {
+            $pagination .= " <a href='catalog.php?";
+            if (!empty($section)){
+                $pagination .= "cat=".$section."&";
+            }
+            $pagination .= "pg=$i'>$i</a>";
+        }
+    }
+
+$pagination .= "</div>";
 
 
 include("inc/header.php"); ?>
 
-<div class="section catalog page">
-    
-    <div class="wrapper">
-        
-        <h1><?php 
-        if ($section != null) {
-            echo "<a href='catalog.php'>Full Catalog</a> &gt; ";
-        }
-        echo $pageTitle; ?></h1>
-        
-        <ul class="items">
-            <?php
-            foreach ($catalog as $item) {
-                echo get_item_html($item);
-            }
-            ?>
-        </ul>
-        
+    <div class="section catalog page">
+
+        <div class="wrapper">
+
+            <h1><?php
+                if ($section != null) {
+                    echo "<a href='catalog.php'>Full Catalog</a> &gt; ";
+                }
+                echo $pageTitle; ?></h1>
+            
+            <?php echo $pagination; ?>
+            <ul class="items">
+                <?php
+                foreach ($catalog as $item) {
+                    echo get_item_html($item);
+                }
+                ?>
+            </ul>
+            <?php echo $pagination; ?>
+        </div>
     </div>
-</div>
 
 <?php include("inc/footer.php"); ?>
--- inc/functions.php
+++ inc/functions.php
@@ -1,149 +1,168 @@
 <?php
-
-function full_catalog_array(){
+/* For pagination. Count all the media per category */
+function get_catalog_count($category = null) {
+    $category = strtolower($category);
     include("connection.php");
 
-    // Try to query the database and make the result into a POD statment object
     try {
-        $results = $db->query("
-            SELECT media_id, title, category, img 
-            FROM Media
-            ORDER BY 
-            REPLACE(
-                REPLACE(
-                  REPLACE(title, 'The ', ''),
-                  'An ',
-                  ''
-                ),
-                'A ',
-                ''
-            )"
-        );
-    } 
-
-    // If can't query the database return an error message
-    catch (Exception $e) {
-        echo "Unable to retrieve results";
+        $sql = "SELECT COUNT(media_id) FROM Media";
+        if (!empty($category)) {
+            $result = $db->prepare(
+                $sql
+                . " WHERE LOWER(category) = ?"
+            );
+            $result->bindParam(1,$category,PDO::PARAM_STR);
+        } else {
+            $result = $db->prepare($sql);
+        }
+        $result->execute();
+    } catch (Exception $e) {
+        echo "bad query";
     }
 
+    $count = $result->fetchColumn(0);
+    return $count;
+}
+
+/*Make an array with all the info in the databse */
+function full_catalog_array($limit = null, $offset = 0) {
+    include("connection.php");
+
+    try {
+        $sql = "SELECT media_id, title, category,img 
+         FROM Media
+         ORDER BY 
+           REPLACE(
+             REPLACE(
+                REPLACE(title,'The ',''),
+                'An ',
+                ''
+             ),
+             'A ',
+             ''
+           )";
+        if (is_integer($limit)) {
+            $results = $db->prepare($sql . " LIMIT ? OFFSET ?");
+            $results->bindParam(1,$limit,PDO::PARAM_INT);
+            $results->bindParam(2,$offset,PDO::PARAM_INT);
+        } else {
+            $results = $db->prepare($sql);
+        }
+        $results->execute();
+    } catch (Exception $e) {
+        echo "Unable to retrieved results";
+        exit;
+    }
 
     $catalog = $results->fetchAll();
     return $catalog;
 }
 
-// Try to query the database and make the result into a POD statment object.  Joins the Media and Genres tables by the genre_id. Will also join Media and Books if book matches media. 
-function single_item_array($id){
+/* query database for only the category int he url param */
+function category_catalog_array($category, $limit = null, $offset = 0) {
+    include("connection.php");
+    $category = strtolower($category);
+    try {
+        $sql = "SELECT media_id, title, category,img 
+         FROM Media
+         WHERE LOWER(category) = ?
+         ORDER BY 
+         REPLACE(
+           REPLACE(
+              REPLACE(title,'The ',''),
+              'An ',
+              ''
+           ),
+           'A ',
+           ''
+         )";
+        if (is_integer($limit)) {
+            $results = $db->prepare($sql . " LIMIT ? OFFSET ?");
+            $results->bindParam(1,$category,PDO::PARAM_STR);
+            $results->bindParam(2,$limit,PDO::PARAM_INT);
+            $results->bindParam(3,$offset,PDO::PARAM_INT);
+        } else {
+            $results = $db->prepare($sql);
+            $results->bindParam(1,$category,PDO::PARAM_STR);
+        }
+        $results->execute();
+    } catch (Exception $e) {
+        echo "Unable to retrieved results";
+        exit;
+    }
+
+    $catalog = $results->fetchAll();
+    return $catalog;
+}
+
+/* make an array of 4 random database objects */
+function random_catalog_array() {
+    include("connection.php");
+
+    try {
+        $results = $db->query(
+            "SELECT media_id, title, category,img 
+         FROM Media
+         ORDER BY RANDOM()
+         LIMIT 4"
+        );
+    } catch (Exception $e) {
+        echo "Unable to retrieved results";
+        exit;
+    }
+
+    $catalog = $results->fetchAll();
+    return $catalog;
+}
+
+/* Try to query the database and make the result into a POD statment object.  Joins the Media and Genres tables by the genre_id. Will also join Media and Books if book matches media. */
+function single_item_array($id) {
     include("connection.php");
 
     try {
         $results = $db->prepare(
-            "SELECT title, category, img, format, year, genre, publisher, isbn
-            FROM Media
-            JOIN Genres ON Media.genre_id = Genres.genre_id
-            LEFT OUTER JOIN Books ON Media.media_id = Books.media_id
-            WHERE Media.media_id = ?"
+            "SELECT title, category, img, format, year, 
+          publisher, isbn, genre
+          FROM Media
+          JOIN Genres ON Media.genre_id=Genres.genre_id
+          LEFT OUTER JOIN Books 
+          ON Media.media_id = Books.media_id
+          WHERE Media.media_id = ?"
         );
         //Use Prepare and ? to protect against SQL injections. Binds the id varable to the ? palceholder
         $results->bindParam(1,$id,PDO::PARAM_INT);
-        //executes the query and loads the results into the results objects
         $results->execute();
-    } 
-
-    catch (Exception $e) {
-        echo "Unable to retrieve results";
+    } catch (Exception $e) {
+        echo "bad query";
+        echo $e;
     }
-
     //Calls the fetch method to retrieve item info for the 1 product that matches the id. loads the item variable. Will contain false if no item found
-    $item = $results->fetch();
-    if (empty($item)) return $item;
-        try {
-            $results = $db->prepare(
-                "SELECT fullname, role
-                FROM Media_People
-                JOIN People ON Media_People.people_id = People.people_id
-                WHERE Media_People.media_id = ?"
-            );
-            //Use Prepare and ? to protect against SQL injections. Binds the id varable to the ? palceholder
-            $results->bindParam(1,$id,PDO::PARAM_INT);
-            //executes the query and loads the results into the results objects
-            $results->execute();
-        } 
+    $item = $results->fetch(PDO::FETCH_ASSOC);
 
-        catch (Exception $e) {
-            echo "Unable to retrieve results";
-        }
-        //loop through people and add them to the correct role in the item array
-        while($row = $results->fetch(PDO::FETCH_ASSOC)){
-            $item[$row["role"]][]= $row["fullname"];
-        }
-        return $item;
-}
-
-function random_catalog_array(){
-    include("connection.php");
-
-    // Try to query the database and make the result into a POD statment object
     try {
-        $results = $db->query("
-            SELECT media_id, title, category, img 
-            FROM Media
-            ORDER BY RANDOM()
-            LIMIT 4"
-        );
-    } 
-
-    // If can't query the database return an error message
-    catch (Exception $e) {
-        echo "Unable to retrieve results";
+        $result = $db->prepare("
+              SELECT fullname,role
+              FROM Media_People
+              JOIN People ON Media_People.people_id=People.people_id
+              WHERE media_id = ?");
+        $result->bindParam(1,$id,PDO::PARAM_INT);
+        $result->execute();
+    } catch (Exception $e) {
+        echo "bad query";
+        echo $e;
     }
-
-
-    $catalog = $results->fetchAll();
-    return $catalog;
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        $item[$row["role"]][] = $row["fullname"];
+    }
+    return $item;
 }
 
-function category_catalog_array($category){
-    include("connection.php");
-    // strtolower and LOWER in the below sql query will make sure we don't run into case issues
-    $category = strtolower($category);
-
-    // Try to query the database and make the result into a POD statment object
-    try {
-        $results = $db->prepare(
-            "SELECT media_id, title, category, img 
-            FROM Media
-            WHERE LOWER(category) = ?
-            ORDER BY 
-            REPLACE(
-                REPLACE(
-                  REPLACE(title, 'The ', ''),
-                  'An ',
-                  ''
-                ),
-                'A ',
-                ''
-            )"
-        );
-        $results->bindParam(1,$category,PDO::PARAM_STR);
-        $results->execute();
-    } 
-
-    // If can't query the database return an error message
-    catch (Exception $e) {
-        echo "Unable to retrieve results";
-    }
-
-
-    $catalog = $results->fetchAll();
-    return $catalog;
-}
-
-
+/* set query param of item */
 function get_item_html($item) {
     $output = "<li><a href='details.php?id="
-        . $item["media_id"] . "'><img src='" 
-        . $item["img"] . "' alt='" 
-        . $item["title"] . "' />" 
+        . $item["media_id"] . "'><img src='"
+        . $item["img"] . "' alt='"
+        . $item["title"] . "' />"
         . "<p>View Details</p>"
         . "</a></li>";
     return $output;
@@ -151,17 +170,17 @@ function get_item_html($item) {
 
 function array_category($catalog,$category) {
     $output = array();
-    
+
     foreach ($catalog as $id => $item) {
         if ($category == null OR strtolower($category) == strtolower($item["category"])) {
             $sort = $item["title"];
             $sort = ltrim($sort,"The ");
             $sort = ltrim($sort,"A ");
             $sort = ltrim($sort,"An ");
-            $output[$id] = $sort;            
+            $output[$id] = $sort;
         }
     }
-    
+
     asort($output);
     return array_keys($output);
 }
